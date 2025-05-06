7d3ab5b06919f6c034a465b1e54a899de12001a6
SadikshyaAcharya39 <acharya.sadikshya12@gmail.com>
2025-05-06 16:18:49 +0545

    SVG Elements, Slider, Double Click

--- main.py
+++ main.py
@@ -132,26 +132,40 @@ driver.get("https://testautomationpractice.blogspot.com/")
 # # date_value = date_picker3.get_attribute("value")
 # # assert date_value != "", "Date was not picked !!!"
 # # assert date_value == "05/02/2025", f"Expected date not picked! Got: {date_value}"
+
+#  Upload File
+# 1. Single File
+
+# singleFile_upload = driver.find_element(By.XPATH, "//input[@id='singleFileInput']")
+# singleFile_upload.click()
 #
-# #  Upload File
-# # 1. Single File
+# file_name = "Remaining Sections"
+# singleFile_upload.send_keys(file_name)
 #
-# # singleFile_upload = driver.find_element(By.XPATH, "//input[@id='singleFileInput']")
-# # singleFile_upload.click()
-# #
-# # file_name = "Remaining Sections"
-# # singleFile_upload.send_keys(file_name)
-# #
-# # uploadSingleFileButton = driver.find_element(By.XPATH, "//button[text()='Upload Single File']")
-# # uploadSingleFileButton.click()
+# uploadSingleFileButton = driver.find_element(By.XPATH, "//button[text()='Upload Single File']")
+# uploadSingleFileButton.click()
 #
 #
-# # uploadButton = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='singleFileInput']")))
-# # WebDriverWait(driver, 10).until(
-# #     EC.element_to_be_clickable((By.XPATH, "//input[@id='singleFileInput']")))
-# # uploadButton.send_keys("C:\\Users\\Sadikshya_Acharya\\Documents\\Remaining_Sections.txt")
-# # driver.find_element(By.XPATH, "//button[text()='Upload Single File']").click()
+# uploadButton = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='singleFileInput']")))
+# WebDriverWait(driver, 10).until(
+#     EC.element_to_be_clickable((By.XPATH, "//input[@id='singleFileInput']")))
+# uploadButton.send_keys("C:\\Users\\Sadikshya_Acharya\\Documents\\Remaining_Sections.txt")
+# driver.find_element(By.XPATH, "//button[text()='Upload Single File']").click()
+
+
+# # Wait until the file input is clickable
+# file_input = WebDriverWait(driver, 10).until(
+#     EC.element_to_be_clickable((By.XPATH, "//input[@id='singleFileInput']"))
+# )
 #
+# # Send the full file path to the file input element
+# file_path = r"C:\Users\Sadikshya_Acharya\Documents\Remaining_Sections.txt"
+# file_input.send_keys(file_path)
+#
+# # Click the "Upload" button
+# upload_button = driver.find_element(By.XPATH, "//button[text()='Upload Single File']")
+# upload_button.click()
+
 # # Pagination Web Table
 #
 # for i in range(2):
@@ -314,149 +328,149 @@ driver.get("https://testautomationpractice.blogspot.com/")
 #
 # alert1.accept()
 #
-# #  Alert2
-# #  Confirmation Alert
-#
-# alert2 = driver.find_element(By.XPATH, "//button[@id='confirmBtn']")
-# alert2.click()
-#
-# # Switching to Alert
-# alert2 = driver.switch_to.alert
-# # alert2.accept()
-#
-# alert2.dismiss()
-# message = driver.find_element(By.XPATH, "//p[@id='demo']")
-# message.is_displayed()
-#
-# body = driver.find_element(By.TAG_NAME, "body")
-# body.send_keys(Keys.PAGE_DOWN)
-#
-# # Alert 3
-# # Prompt Alert
-#
-# alert3 = driver.find_element(By.XPATH, "//button[@id='promptBtn']")
-# alert3.click()
-#
-# # Switching to Alert
-# alert = driver.switch_to.alert
-# alert.send_keys("Sadikshya!")
-# # alert.accept()
-# alert.dismiss()
-#
-# # New Tab
-#
-# # Storing the number of original tabs
-# original_tabs = driver.window_handles
-# original_tab_count = len(original_tabs)
-#
-# newTab_button = driver.find_element(By.XPATH, "//button[text()='New Tab']")
-# newTab_button.click()
-#
-# updated_tabs = driver.window_handles
-# updated_tab_count = len(updated_tabs)
+#  Alert2
+#  Confirmation Alert
+
+alert2 = driver.find_element(By.XPATH, "//button[@id='confirmBtn']")
+alert2.click()
+
+# Switching to Alert
+alert2 = driver.switch_to.alert
+# alert2.accept()
+
+alert2.dismiss()
+message = driver.find_element(By.XPATH, "//p[@id='demo']")
+message.is_displayed()
+
+body = driver.find_element(By.TAG_NAME, "body")
+body.send_keys(Keys.PAGE_DOWN)
+
+# Alert 3
+# Prompt Alert
+
+alert3 = driver.find_element(By.XPATH, "//button[@id='promptBtn']")
+alert3.click()
+
+# Switching to Alert
+alert = driver.switch_to.alert
+alert.send_keys("Sadikshya!")
+# alert.accept()
+alert.dismiss()
+
+# New Tab
+
+# Storing the number of original tabs
+original_tabs = driver.window_handles
+original_tab_count = len(original_tabs)
+
+newTab_button = driver.find_element(By.XPATH, "//button[text()='New Tab']")
+newTab_button.click()
+
+updated_tabs = driver.window_handles
+updated_tab_count = len(updated_tabs)
+
+# Assertions
+assert updated_tab_count > original_tab_count, "New tab was not opened!"
+print("New tab opened successfully...")
+
+# Switching to a new tab
+driver.switch_to.window(updated_tabs[-1])
+
+# Handling Popup Windows
+
+# Selenium treats both tabs and pop up windows in same way
+
+ # Current window handles
+original_window = driver.window_handles
+original_count = len(original_window)
+
+
+popUp_window = driver.find_element(By.XPATH, "//div[@class='column']")
+popUp_window.click()
+time.sleep(2)
+
+
+new_windows = driver.window_handles
+new_count = len(new_windows)
 #
 # # Assertions
-# assert updated_tab_count > original_tab_count, "New tab was not opened!"
-# print("New tab opened successfully...")
-#
-# # Switching to a new tab
-# driver.switch_to.window(updated_tabs[-1])
-#
-# # Handling Popup Windows
-#
-# # Selenium treats both tabs and pop up windows in same way
-#
-#  # Current window handles
-# original_window = driver.window_handles
-# original_count = len(original_window)
-#
-#
-# popUp_window = driver.find_element(By.XPATH, "//div[@class='column']")
-# popUp_window.click()
-# time.sleep(2)
-#
-#
-# new_windows = driver.window_handles
-# new_count = len(new_windows)
-# #
-# # # Assertions
-# assert new_count > original_count, "Pop-up window did not open"
-# print("New window opened successfully")
-#
-# body = driver.find_element(By.TAG_NAME, "body")
-# body.send_keys(Keys.PAGE_DOWN)
-# body = driver.find_element(By.TAG_NAME, "body")
-# body.send_keys(Keys.PAGE_DOWN)
-#
-# # Handling Mouse Hover
-# point_me_button = driver.find_element(By.XPATH, "//div[@class='dropdown']")
-# driver.execute_script("arguments[0].scrollIntoView(true);", point_me_button)
-#
-# # Wait until the element is visible
-# WebDriverWait(driver, 10).until(EC.visibility_of(point_me_button))
-#
-# # Using ActionChain Object
-# actions = ActionChains(driver)
-# actions.move_to_element(point_me_button).perform()
-#
-# mobiles_button = driver.find_element(By.XPATH, "//a[text()='Mobiles']")
-# mobiles_button.click()
+assert new_count > original_count, "Pop-up window did not open"
+print("New window opened successfully")
+
+body = driver.find_element(By.TAG_NAME, "body")
+body.send_keys(Keys.PAGE_DOWN)
+body = driver.find_element(By.TAG_NAME, "body")
+body.send_keys(Keys.PAGE_DOWN)
+
+# Handling Mouse Hover
+point_me_button = driver.find_element(By.XPATH, "//div[@class='dropdown']")
+driver.execute_script("arguments[0].scrollIntoView(true);", point_me_button)
+
+# Wait until the element is visible
+WebDriverWait(driver, 10).until(EC.visibility_of(point_me_button))
+
+# Using ActionChain Object
+actions = ActionChains(driver)
+actions.move_to_element(point_me_button).perform()
+
+mobiles_button = driver.find_element(By.XPATH, "//a[text()='Mobiles']")
+mobiles_button.click()
 
 # Handling Drag and Drop
 
-# drag_locator = driver.find_element(By.XPATH, "//p[text()='Drag me to my target']")
-#
-# droppable_locator = driver.find_element(By.XPATH, "//div[@id='droppable']")
-#
-# driver.execute_script("arguments[0].scrollIntoView(true);", drag_locator)
-# driver.execute_script("arguments[0].scrollIntoView(true);", droppable_locator)
-#
-# # Creating action chain object and perform drag and drop
-# actions = ActionChains(driver)
-# actions.drag_and_drop(drag_locator, droppable_locator).perform()
+drag_locator = driver.find_element(By.XPATH, "//p[text()='Drag me to my target']")
+
+droppable_locator = driver.find_element(By.XPATH, "//div[@id='droppable']")
+
+driver.execute_script("arguments[0].scrollIntoView(true);", drag_locator)
+driver.execute_script("arguments[0].scrollIntoView(true);", droppable_locator)
+
+# Creating action chain object and perform drag and drop
+actions = ActionChains(driver)
+actions.drag_and_drop(drag_locator, droppable_locator).perform()
 
 #  Getting the value of Static Web Table
 
-for i in range(4):
+for i in range(2):
  body = driver.find_element(By.TAG_NAME, "body")
  body.send_keys(Keys.PAGE_DOWN)
-#
-# web_table = driver.find_element(By.XPATH, "//div[@id='HTML1']")
-# assert web_table.is_displayed(), "Static Web Table is not displayed.."
-#
-# # Book Name
-# # book_name = driver.find_element(By.XPATH, "//table//tr[4]/td[contains(text(), 'Learn JS')]")
-# # assert book_name.is_displayed(), "Book name is not displayed.."
-# # print(book_name.text)
-#
-# # Author
-# author_name = driver.find_element(By.XPATH, "//table//tr[5]/td[contains(text(), 'Mukesh')]")
-# assert author_name.is_displayed(), "Author name is not displayed.."
-# print(author_name.text)
-#
-# # Subject
-# subject_name = driver.find_element(By.XPATH, "//table//tr[7]/td[contains(text(), 'Javascript')]")
-# assert subject_name.is_displayed(), "Subject name is not displayed.."
-# print(subject_name.text)
-#
-# # Price
-# price = driver.find_element(By.XPATH, "//table//tr[5]/td[contains(text(), '3000')]")
-# assert price.is_displayed(), "price is not displayed.."
-# print(price.text)
+
+web_table = driver.find_element(By.XPATH, "//div[@id='HTML1']")
+assert web_table.is_displayed(), "Static Web Table is not displayed.."
+
+# Book Name
+book_name = driver.find_element(By.XPATH, "//table//tr[4]/td[contains(text(), 'Learn JS')]")
+assert book_name.is_displayed(), "Book name is not displayed.."
+print(book_name.text)
+
+# Author
+author_name = driver.find_element(By.XPATH, "//table//tr[5]/td[contains(text(), 'Mukesh')]")
+assert author_name.is_displayed(), "Author name is not displayed.."
+print(author_name.text)
+
+# Subject
+subject_name = driver.find_element(By.XPATH, "//table//tr[7]/td[contains(text(), 'Javascript')]")
+assert subject_name.is_displayed(), "Subject name is not displayed.."
+print(subject_name.text)
+
+# Price
+price = driver.find_element(By.XPATH, "//table//tr[5]/td[contains(text(), '3000')]")
+assert price.is_displayed(), "price is not displayed.."
+print(price.text)
 
 #  Verify the number of rows and columns
 #  Using Web Table
 
-# web_table = driver.find_elements(By.XPATH, "//table[@name='BookTable']")
-#
-# # Access the first table in the list
-# first_table = web_table[0]
-#
-# rows = first_table.find_elements(By.TAG_NAME, "tr")
-# print("Total number of rows: " , len(rows))
-#
-# header_column = rows[0].find_elements(By.TAG_NAME, "th")
-# print("Total number of columns: " , len(header_column))
+web_table = driver.find_elements(By.XPATH, "//table[@name='BookTable']")
+
+# Access the first table in the list
+first_table = web_table[0]
+
+rows = first_table.find_elements(By.TAG_NAME, "tr")
+print("Total number of rows: " , len(rows))
+
+header_column = rows[0].find_elements(By.TAG_NAME, "th")
+print("Total number of columns: " , len(header_column))
 
 # Using Loop
 # web_tables = driver.find_elements(By.XPATH, "//table[@name='BookTable']")
@@ -469,13 +483,13 @@ scrolling_dropdown = driver.find_element(By.XPATH, "//input[@id='comboBox']")
 scrolling_dropdown.click()
 
 items = driver.find_elements(By.XPATH, "//div[@id='dropdown']/div")
-#  Can't use select becuase dropdown is not using Select tag.
+ # Can't use select because dropdown is not using Select tag.
 
-# select = Select(items)
-#
-# #  By visible text
-# select.select_by_visible_text("Item 97")
-# scrolling_dropdown.is_displayed(), "Selected Item is not displayed.."
+select = Select(items)
+
+#  By visible text
+select.select_by_visible_text("Item 97")
+scrolling_dropdown.is_displayed(), "Selected Item is not displayed.."
 #
 
 #  Using Loop
@@ -486,3 +500,121 @@ for item in items:
 
 # Verify that the item is selected
 assert scrolling_dropdown.is_displayed(), "Dropdown is not displayed."
+
+
+# SVG Elements
+
+svg_container = driver.find_element(By.XPATH, "//div[@class='svg-container']")
+
+circle = driver.find_element(By.XPATH, "//*[local-name()='svg']//*[local-name()='circle']")
+circle.is_displayed(), "Circle is not displayed.."
+
+assert circle.get_attribute("cx") == 15  # is incorrect
+assert circle.get_attribute("cy") == 15
+assert circle.get_attribute("r") == 7
+
+rectangle = driver.find_element(By.XPATH, "//*[local-name()='svg']//*[local-name()='rect']")
+rectangle.is_displayed(), "Rectangle is not displayed.."
+
+assert int(rectangle.get_attribute("x")) == 3
+assert int(rectangle.get_attribute("y")) == 5
+assert int(rectangle.get_attribute("width")) == 24
+assert int(rectangle.get_attribute("height")) == 5  # is incorrect
+
+# Slider
+# Price Range
+
+# Original Range
+
+original_price_range = driver.find_element(By.XPATH, "//input[@id='amount']").get_attribute("value")
+
+# price_range = driver.find_element(By.XPATH, "//div[@class='ui-slider-range ui-corner-all ui-widget-header']")
+
+slider1 = driver.find_element(By.XPATH, "//span[@class='ui-slider-handle ui-corner-all ui-state-default'][1]")
+
+
+slider2 = driver.find_element(By.XPATH, "//span[@class='ui-slider-handle ui-corner-all ui-state-default'][2]")
+
+ # Using action chain object
+actions = ActionChains(driver)
+actions.click_and_hold(slider1).move_by_offset(50,0).release().perform()
+actions.click_and_hold(slider2).move_by_offset(0, 50).release().perform()
+
+changed_price = driver.find_element(By.XPATH, "//input[@id='amount']").get_attribute("value")
+assert original_price_range != changed_price, "Original price range is equal to changed price"
+
+
+ # Double Click
+
+textfield1 = driver.find_element(By.XPATH, "//input[@id='field1']")
+value1 = textfield1.get_attribute("value")
+
+# copy_text_button = driver.find_element(By.XPATH, "//button[text()='Copy Text']")
+
+copy_text_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[text()='Copy Text']"))
+)
+
+# Scroll the button into view
+driver.execute_script("arguments[0].scrollIntoView(true);", copy_text_button)
+
+actions = ActionChains(driver)
+actions.double_click(copy_text_button).perform()
+
+
+# Wait or sleep briefly if DOM updates asynchronously
+WebDriverWait(driver, 5).until(
+    lambda d: d.find_element(By.XPATH, "//input[@id='field2']").get_attribute("value") != ""
+)
+
+
+textfield2 = driver.find_element(By.XPATH, "//input[@id='field2']")
+assert textfield1.get_attribute("value") == textfield2.get_attribute("value"), "Text was not copied!!!"
+
+textfield1 = driver.find_element(By.XPATH, "//input[@id='field1']")
+full_text = textfield1.get_attribute("value")
+
+trimmed_text = full_text.split(" ")[0]
+print(trimmed_text)
+
+textfield1.clear()
+textfield1.send_keys(trimmed_text , " Sadikshya ")
+
+
+ # Hidden Elements and AJAX
+original_url = driver.current_url
+
+link_text = driver.find_element(By.XPATH, "//a[text()='Hidden Elements & AJAX']")
+link_text.click()
+
+changed_url = driver.current_url
+
+assert original_url != changed_url, "Original URL should not be same as Changed URL.."
+time.sleep(5)
+
+
+input_box2 = driver.find_element(By.XPATH, "//button[@id='toggleInput']")
+input_box2.is_enabled(), "Input box2 is not enabled!"
+
+input_box2.click()
+time.sleep(3)
+
+input2box = driver.find_element( By.XPATH , "//input[@id='input2']")
+input2box.is_displayed(), "Is not displayed!!!"
+
+# Toogle CheckBox button
+checkbox_button = driver.find_element(By.XPATH, "//button[@id='toggleCheckbox']")
+checkbox_button.click()
+
+checkbox = driver.find_element(By.XPATH, "//input[@id='checkbox2']")
+assert checkbox.is_displayed(), "Is not displayed.."
+
+
+
+
+
+
+
+
+
+
